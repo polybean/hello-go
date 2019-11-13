af2f43a780b1b0d44e55e0a290ffdbd365d5e6cc
polybean <polybean@gmail.com>
2019-11-13 16:29:14 +0800

    Go->CDP

--- src/main.go
+++ src/main.go
@@ -19,7 +19,7 @@ type greeting struct {
 }
 
 func index(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello, Go!")
+	fmt.Fprintf(writer, "Hello, CDP!")
 }
 
 func findAllGreetings(writer http.ResponseWriter, request *http.Request) {
